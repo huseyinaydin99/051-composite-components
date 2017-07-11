fa5dad33db9f251e404c209449bf5a021163a24c
huso51 <huseyinaydin99@gmail.com>
2017-07-11 13:55:49 +0300

    Add files via upload

---
+++ 051_CompositeComponents/src/java/com/siteadi/YonetimliBean.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.siteadi;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author husey
+ */
+@ManagedBean
+@RequestScoped
+public class YonetimliBean {
+
+    /**
+     * Creates a new instance of YonetimliBean
+     */
+    public YonetimliBean() {
+    }
+    public String adiSoyadi,kullaniciAdi,parola;
+
+    public String getAdiSoyadi() {
+        return adiSoyadi;
+    }
+
+    public void setAdiSoyadi(String adiSoyadi) {
+        this.adiSoyadi = adiSoyadi;
+    }
+
+    public String getKullaniciAdi() {
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi) {
+        this.kullaniciAdi = kullaniciAdi;
+    }
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+    
+    public String kayitOlActionMetodu(){
+        return "sonuc";
+    }
+}
